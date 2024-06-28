a2be57154370901337578beca9ad0891e97cc140
wan <106522971+wan0911@users.noreply.github.com>
2024-06-28 16:24:34 +0900

    Top Competitors 풀이2

--- SQL(oracle)/HackerRank/Top Competitors.sql
+++ SQL(oracle)/HackerRank/Top Competitors.sql
@@ -5,6 +5,8 @@
 -- 4. ORDER BY
 */
 
+
+-- 1) JOIN 없이
 SELECT H.hacker_id, H.name
     FROM (SELECT S.hacker_id, COUNT(1) AS CNT
                   FROM Submissions S
@@ -23,3 +25,18 @@ SELECT H.hacker_id, H.name
 ORDER
         BY A.CNT DESC, H.hacker_id;
 
+
+
+
+-- 2) JOIN O
+-- JOIN으로 푸는게 좋을듯.. WHERE -> GROUP BY를 걸면 된다 (GROUP BY 사용 더 생각하기)
+SELECT S.hacker_id, H.name
+  FROM Submissions S
+    JOIN Challenges C   ON C.challenge_id = S.challenge_id
+    JOIN Difficulty D       ON D.difficulty_level = C.difficulty_level  
+    JOIN Hackers H        ON H.hacker_id = S.hacker_id
+WHERE S.score = D.score
+GROUP
+        BY S.hacker_id, H.name
+HAVING COUNT(S.hacker_id) >= 2
+ORDER BY COUNT(S.hacker_id) DESC, S.hacker_id;
